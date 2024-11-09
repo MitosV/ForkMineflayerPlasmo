b822363e29a170a42a65f778a94e7b5ef90c4928
MitosV <mitosv@proton.me>
2024-11-08 21:08:38 -0600

    run script

---
+++ run.py
@@ -0,0 +1,26 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import subprocess
+
+def run_command(thread_id):
+    command = f'node ./lib/index.js mitos_voice_{thread_id}'
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        result.check_returncode()  
+        return f"Hilo {thread_id}: {result.stdout.strip()}"
+    except subprocess.CalledProcessError as e:
+        return f"Hilo {thread_id} falló: {e}"
+
+def main():
+    num_threads = 25
+
+    # Crear un ThreadPoolExecutor para manejar los hilos
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        # Enviar todas las tareas al executor para que se ejecuten al mismo tiempo
+        futures = [executor.submit(run_command, i) for i in range(num_threads)]
+
+        for future in as_completed(futures):
+            print(future.result())
+
+if __name__ == "__main__":
+    main()
+
